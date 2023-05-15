e76ba47d8897630937d3dada2f4615db440b7c97
Lucas Montecino <lucasmontecino95@gmail.com>
2023-05-15 17:38:22 -0300

    new backend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import DogCreate from "./components/DogCreate";
 import DogDetail from "./components/DogDetail";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://deploy-production-fd67.up.railway.app/";
 
 function App() {
   return (
