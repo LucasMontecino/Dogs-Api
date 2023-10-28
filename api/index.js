cec7d6f6feae0e86435663ddaff260fb8d3cf88f
Lucas Montecino <lucasmontecino95@gmail.com>
2023-10-28 11:07:56 -0300

    initial-index-file

--- api/index.js
+++ api/index.js
@@ -17,12 +17,54 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require("./src/app.js");
+// const server = require("./src/app.js");
 require("dotenv").config();
 
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
 const port = PORT || 3001;
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./src/routes/index.js");
+
+require("./src/db.js");
+
+const server = express();
+const cors = require("cors");
+
+server.name = "API";
+
+const apiRef = "https://dogs-api-lilac.vercel.app";
+
+server.use(cors());
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `*`); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.use("/", routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
@@ -30,3 +72,5 @@ conn.sync({ force: false }).then(() => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
+
+module.exports = server;
