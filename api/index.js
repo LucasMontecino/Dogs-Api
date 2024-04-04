3b0e181a01c01391eff5c8578eff955cf83f6606
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-04 09:08:52 -0300

    implement-deploy

--- api/index.js
+++ api/index.js
@@ -38,19 +38,19 @@ conn.sync({ force: false }).then(() => {
 require("./src/db.js");
 
 const server = express();
-// const cors = require("cors");
+const cors = require("cors");
 
 server.name = "API";
 
 const apiRef = "https://dogs-api-lilac.vercel.app";
 
-// server.use(cors());
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `*`); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", `${apiRef}`); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
