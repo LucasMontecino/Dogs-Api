f78574d3a31a09cfdb1d1b37cb341e407373fdeb
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-04 09:15:51 -0300

    adding-api-rest

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import DogCreate from "./components/DogCreate";
 import DogDetail from "./components/DogDetail";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://dogs-api-gkk4.onrender.com";
+axios.defaults.baseURL = "https://dogs-api-rest.vercel.app/";
 // axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
