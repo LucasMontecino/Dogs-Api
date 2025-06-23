0e33fdc138ac4fb69263f42054a5e852cd268336
Lucas Montecino <lucasmontecino95@gmail.com>
2025-06-23 18:43:46 -0300

    feat: create config, logger and middleware files

---
+++ api/src/utils/config.js
@@ -0,0 +1,19 @@
+const {
+  PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DATABASE_URL,
+} = process.env;
+
+module.exports = {
+  PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DATABASE_URL,
+};
