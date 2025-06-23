51afe16112e851fbefa3cfcff3e6a9c8cee6924a
Lucas Montecino <lucasmontecino95@gmail.com>
2025-06-23 19:11:17 -0300

    refactor: update url

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    `https://dogs-api-lilac.vercel.app/`
+    `https://dogs-api-lilac.vercel.app`
   ); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
