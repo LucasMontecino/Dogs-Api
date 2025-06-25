b4df3c6c720b18a6a14dceda52db6c8654517cb2
Lucas Montecino <lucasmontecino95@gmail.com>
2025-06-25 15:26:58 -0300

    fix: update missing slash from uri

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'https://dogs-api-rest.vercel.app/';
+    : 'https://dogs-api-rest.vercel.app';
 
 export default API_URL;
